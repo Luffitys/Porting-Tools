82131b3f2b57c68e5cc4353ead566a58557edd05
luffitys <banackidaniel@gmail.com>
2021-04-04 23:36:47 +0200

    Improve Decompile Script

--- Porting-Tools/Scripts/2-Decompile.bat
+++ Porting-Tools/Scripts/2-Decompile.bat
@@ -11,7 +11,6 @@ set /p REQUIRES_MAGISK_MODULE="Is a Magisk Module for the modded APK required? (
 
 set ROM=ROM\ROM
 set APKTOOL=Tools\APKTool
-set APKPATH=%APP_OR_PRIV-APP%\%APKNAME%\%APKNAME%.apk
 
 cd ..
 
@@ -20,7 +19,7 @@ if /I "%REQUIRES_MAGISK_MODULE%"=="y" (robocopy Tools\Magisk_Template ..\%APKNAM
 @echo on
 
 	:: Decompile APP
-java -jar %APKTOOL%\apktool.jar d --no-debug-info --keep-broken-res --output ..\%APKNAME_NEW%_APK %ROM%\%PARTITION%\%APKPATH% -p %APKTOOL%\Frameworks
+java -jar %APKTOOL%\apktool.jar d --no-debug-info --keep-broken-res --output ..\%APKNAME_NEW%_APK %ROM%\%PARTITION%\%APP_OR_PRIV-APP%\%APKNAME%\%APKNAME%.apk -p %APKTOOL%\Frameworks
 
 	:: Avoid cmd closing after finish to see eventual issues
 pause
